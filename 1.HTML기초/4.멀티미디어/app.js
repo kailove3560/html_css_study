74e5a60742ad9c1aacf389fdbfb06187b3945534
kailove3560 <lsw3560@naver.com>
2021-02-01 15:41:11 +0900

    [21.02.01] #HTML 학습 완료

--- 1.HTML기초/4.멀티미디어/app.js
+++ 1.HTML기초/4.멀티미디어/app.js
@@ -4,5 +4,5 @@ const $btn = document.querySelector('#change');
 const $pet = document.querySelector('#my-pet');
 
 $btn.addEventListener('click', e => {
-    $pet.textContent = '못난이 거북이!!';
+    $pet.textContent = '못난이 거북이!!'; /* 자바스크립트 주석 */ 
 });
